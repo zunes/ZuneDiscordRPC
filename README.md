188755e47e146498247601a3a733ea6319c72b8a
Lars <lars@pfrenger.com>
2022-01-24 19:54:01 +0100

    Added Try Icon and Cover art in Discord!

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # ZuneDiscordRPC
 
 ![Discord Screenshot](/screenshots/discord.png)
+
+## Changelog
+v0.2:
+    - Added Tray Icon
+    - Added Album Art in Discord using Discogs API
+v0.1:
+    - Initial Release
+    - Artist, Album, Title shown in Discord
---
+++ ZuneDiscordRPC/resource.h
@@ -0,0 +1,19 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by Resource.rc
+//
+#define IDI_ICON                        102
+#define IDR_CONTEXT_MENU                105
+#define ID_ZUNEDISCORDRPC_SHOWCONSOLE   40003
+#define ID_ZUNEDISCORDRPC_EXIT          40004
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        106
+#define _APS_NEXT_COMMAND_VALUE         40005
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
